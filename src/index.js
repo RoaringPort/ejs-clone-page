40ccc7ca7e85c26242dc4f5e35bb16eb25845326
Your Name <you@example.com>
2020-07-01 09:44:35 -0500

    se inicia el archivo src/views/partials/nav y src/views/partials/header, se crean los modulso de acceso de las distantas partes de la pagina web y se ponen en la ruta del archivo index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
+app.get('/eventos', (req, res) => {
+    res.render('eventos.ejs')
+})
+app.get('/pagos', (req, res) => {
+    res.render('pagos.ejs')
+})
+app.get('/inicio', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/productos', (req, res) => {
+    res.render('products.ejs')
+})
 
 app.listen(port, () => {
     console.log("Servidor en el puerto: " , port)
